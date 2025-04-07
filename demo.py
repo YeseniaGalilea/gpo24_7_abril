c8b4170b82db600539569945de5eb1db185eef52
Yesenia Galilea Alvarez Mendoza <yesenia.15galilea@gmail.com>
2025-04-07 15:41:21 -0600

    codigo finalizado

--- demo.py
+++ demo.py
@@ -1,2 +1,11 @@
 class Cuadrado:
-    pass
+    def __init__(self, lado):
+        self.lado = lado 
+        
+    def area (self):
+         return self.lado **2 
+        
+        
+lado1 = int(input("Ingresa un lado: "))
+mi_cuadrado= Cuadrado(lado1)
+print("el area del cuadrado es: ", mi_cuadrado.area())
